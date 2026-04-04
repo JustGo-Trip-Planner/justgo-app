5910253e35bc6dd2da3bf5d9183c678f1c293e47
bbiiw <wongwiwat.2004@gmail.com>
2026-04-05 02:45:52 +0700

    feat: update configuration

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -1,60 +1,50 @@
 import "dotenv/config";
+
 export default {
-  "expo": {
-    "name": "mobile",
-    "slug": "mobile",
-    "version": "1.0.0",
-    "extra": {
+  expo: {
+    name: "mobile",
+    slug: "mobile",
+    version: "1.0.0",
+    extra: {
+      eas: {
+        projectId: "ac3814e6-0d11-4a07-87dd-1d5492a916d9"
+      },
       API_URL: process.env.API_URL,
       GOOGLE_MAPS_API_KEY: process.env.GOOGLE_MAPS_API_KEY,
     },
-    "orientation": "portrait",
-    // "icon": "./assets/images/icon.png",
-    "scheme": "mobile",
-    "userInterfaceStyle": "automatic",
-    "newArchEnabled": true,
-    "ios": {
-      "supportsTablet": true,
-      "infoPlist": {
-        "NSAppTransportSecurity": {
-          "NSAllowsArbitraryLoads": true
-        }
+    orientation: "portrait",
+    scheme: "mobile",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.wongwiwat.justgo",
+      infoPlist: {
+        NSAppTransportSecurity: {
+          NSAllowsArbitraryLoads: true
+        },
+        ITSAppUsesNonExemptEncryption: false
       }
     },
-    "android": {
-      "adaptiveIcon": {
-        "backgroundColor": "#E6F4FE",
-        "foregroundImage": "./assets/images/android-icon-foreground.png",
-        "backgroundImage": "./assets/images/android-icon-background.png",
-        "monochromeImage": "./assets/images/android-icon-monochrome.png"
+    android: {
+      package: "com.wongwiwat.justgo",
+      adaptiveIcon: {
+        backgroundColor: "#E6F4FE",
       },
-      "edgeToEdgeEnabled": true,
-      "predictiveBackGestureEnabled": false,
-      "usesCleartextTraffic": true
+      edgeToEdgeEnabled: true,
+      predictiveBackGestureEnabled: false,
+      usesCleartextTraffic: true
     },
-    "web": {
-      "output": "static",
-      "favicon": "./assets/images/favicon.png"
+    web: {
+      output: "static",
     },
-    "plugins": [
+    plugins: [
       "expo-router",
-      [
-        "expo-splash-screen",
-        {
-          "image": "./assets/images/splash-icon.png",
-          "imageWidth": 200,
-          "resizeMode": "contain",
-          "backgroundColor": "#ffffff",
-          "dark": {
-            "backgroundColor": "#000000"
-          }
-        }
-      ],
       "expo-font"
     ],
-    "experiments": {
-      "typedRoutes": true,
-      "reactCompiler": true
+    experiments: {
+      typedRoutes: true,
+      reactCompiler: true
     }
   }
-}
+};
